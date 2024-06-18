89c05b70ebba5b5ae6c2115805af4439752645b2
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-06-18 16:27:15 +0500

    chat qoshildi

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -2,31 +2,12 @@ import EditScreenInfo from '@/components/EditScreenInfo'
 import { View } from '@/components/Themed'
 import React from 'react'
 import { StyleSheet, Text } from 'react-native'
+import ChatSupport from '../(chat)'
 
 function ChatScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab Two</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <EditScreenInfo path="app/(tabs)/two.tsx" />
-        </View>
+        <ChatSupport/>
     )
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-  });
 
 export default ChatScreen   
