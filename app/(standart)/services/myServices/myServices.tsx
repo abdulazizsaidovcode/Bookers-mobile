51d4360c838536ece5869e519db5399b5a7dee48
omadbekolimov <omadbekolimov96@gmail.com>
2024-06-19 16:18:30 +0500

    goo

--- app/(standart)/services/myServices/myServices.tsx
+++ app/(standart)/services/myServices/myServices.tsx
@@ -6,34 +6,29 @@ import { router } from "expo-router"; // router ni import qiling
 import { ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
-
-
-
-
-
+import { useNavigation } from 'expo-router';
 
 const MyServices = () => {
-
-    const handlePress = () => {
-        alert("hello")
-        router.push('/ServesGender'); // router.push orqali sahifani o'zgartirish
-    };
-    
-   
-    
+    const navigation = useNavigation()
     return (
         <SafeAreaView>
             <ScrollView>
                 <View style={tw`flex w-full`}>
                     <NavigationMenu name="Мои услуги" />
                     <View style={tw` p-5`}>
-                        <MyServicess title="Направление услуг по полу" subTitle="Не выбрано" onPress={handlePress} />
-                        <MyServicess title="Hellor" subTitle="Hello" onPress={handlePress} />
-                    </View> 
+                        <MyServicess
+                            title="Направление услуг по полу"
+                            subTitle="Не выбрано"
+                            
+                        />
+                        <MyServicess
+                            title="Hellor"
+                            subTitle="Hello"
+                        />
+                    </View>
                 </View>
-         </ScrollView>
+            </ScrollView>
         </SafeAreaView>
     );
 };
-
 export default MyServices;
--- components/services/myServices.tsx
+++ components/services/myServices.tsx
@@ -9,7 +9,8 @@ import { MyServicesProps } from '@/type/services/myServices';
 const MyServicess: React.FC<MyServicesProps> = ({ title, subTitle, onPress }) => {
     return (
         <View style={tw`mt-3  `}>
-            <TouchableOpacity 
+            <TouchableOpacity
+            activeOpacity={.7}
                 style={tw`bg-white rounded-2xl  p-4 flex-row justify-between items-center`}
                 onPress={onPress}
             >
