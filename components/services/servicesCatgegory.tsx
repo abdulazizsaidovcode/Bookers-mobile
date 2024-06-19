e79f0364cb20ad9b002ebfb0bf4345d20f5341d5
omadbekolimov <omadbekolimov96@gmail.com>
2024-06-19 09:57:59 +0500

    Category bo'ldi

---
+++ components/services/servicesCatgegory.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Text, View } from "../Themed";
+import tw from "tailwind-react-native-classnames";
+import { Checkbox } from 'react-native-paper';
+import { ServicesProps } from '@/type/services/servicesCategory';
+
+
+
+const ServicesCategory: React.FC<ServicesProps> = ({ title, onPress }) => {
+    const [checked, setChecked] = useState(false);
+
+    const handlePress = () => {
+        setChecked(!checked);
+        if (onPress) {
+            onPress();
+        }
+    };
+
+    return (
+        <View style={tw`p-2 bg-gray-100 flex-1`}>
+            <View style={[tw`flex p-3 flex-row items-center rounded-xl bg-white`]}>
+                <View>
+                    <Checkbox
+                        status={checked ? 'checked' : 'unchecked'}
+                        onPress={handlePress}
+                        color={checked ? 'red' : undefined}
+                    />
+                </View>
+                <Text style={tw`text-black text-lg font-bold`}>{title}</Text>
+            </View>
+        </View>
+    );
+}
+
+export default ServicesCategory;
