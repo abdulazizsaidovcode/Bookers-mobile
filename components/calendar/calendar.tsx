ecddea74f37327d9694aa2ada370c20cf312ac44
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-06-19 13:38:37 +0500

    calendar add

--- components/calendar/calendar.tsx
+++ components/calendar/calendar.tsx
@@ -1,12 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-const Calendar = () => {
+const CalendarComponent = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date(2024, 2, 20));
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const onChange = (selectedDate: any) => {
+    const currentDate = selectedDate;
+    setShowCalendar(false);
+    setSelectedDate(currentDate);
+  };
+
+  const formatDate = (date: Date) => {
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    console.log(date.toLocaleDateString('ru-RU', options));
+    
+    return date.toLocaleDateString('ru-RU', options);
+  };
+  
   return (
-    <View>
-      <Text>Calendar</Text>
-    </View>
-  )
-}
+    <>
+      <TouchableOpacity style={styles.datePicker} onPress={() => setShowCalendar(true)}>
+        <Text style={styles.dateText}>{formatDate(selectedDate)}</Text>
+        <View style={styles.calendarIcon} />
+      </TouchableOpacity>
+      {showCalendar && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={selectedDate}
+          mode="date"
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+    </>
+  );
+};
 
-export default Calendar
+const styles = StyleSheet.create({
+  datePicker: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#4B4B64',
+    borderRadius: 10,
+    padding: 10,
+  },
+  dateText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+  },
+  calendarIcon: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#403D4B',
+    borderRadius: 12,
+  },
+});
+
+export default CalendarComponent;
