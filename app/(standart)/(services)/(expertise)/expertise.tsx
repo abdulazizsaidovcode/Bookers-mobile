dc922e189a616d16fc221c6ddb9ccc94c9a9e3e4
omadbekolimov <omadbekolimov96@gmail.com>
2024-06-20 17:00:54 +0500

    services tugadi

--- app/(standart)/(services)/(expertise)/expertise.tsx
+++ app/(standart)/(services)/(expertise)/expertise.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, View, Text, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
@@ -6,71 +6,77 @@ import NavigationMenu from '@/components/navigation/navigation-menu';
 import ServicesCategory from '@/components/services/servicesCatgegory';
 import Buttons from '@/components/(buttons)/button';
 import CenteredModal from '@/components/(modals)/modal-centered';
+import { router, useNavigation } from 'expo-router';
 
-const Expertise = () => {
-    const [modalVisible, setModalVisible] = useState(false);
-    const [textAreaValue, setTextAreaValue] = useState('');
+interface Service {
+title: string;
+}
 
-    console.log(textAreaValue);
+const Expertise: React.FC = () => {
+    const navigation = useNavigation();
+const [modalVisible, setModalVisible] = useState<boolean>(false);
+const [textAreaValue, setTextAreaValue] = useState<string>('');
+const [services, setServices] = useState<Service[]>([
+{ title: "Здоровье и красота волос" },
+{ title: "Ногтевой сервис" },
+{ title: "Ресницы и брови" },
+{ title: "Уход за телом" },
+{ title: "Уход за лицом" }
+]);
+const openModal = () => setModalVisible(true);
+const closeModal = () => setModalVisible(false);
 
-    const services = [
-        { title: "Здоровье и красота волос", },
-        { title: "Ногтевой сервис" },
-        { title: "Ресницы и брови" },
-        { title: "Уход за телом" },
-        { title: "Уход за лицом" }
-    ];
+const handleSave = () => {
+    if (textAreaValue.trim() && textAreaValue !== '' && textAreaValue !== "''") {
+        setServices([...services, { title: textAreaValue }]);
+        setTextAreaValue('');  
+        closeModal();  
+    }
+};
 
-    const openModal = () => setModalVisible(true);
-    const closeModal = () => {
-        setModalVisible(false);
-        textAreaValue
-    };
-
-    const handleSave = () => {
-        textAreaValue
-        closeModal(); 
-    };
-
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <ScrollView style={{ flex: 1 }}>
-                <View style={tw`w-full p-3`}>
-                    <NavigationMenu name="Специализация" />
-                    {services.map((service, index) => (
-                        <ServicesCategory key={index} title={service.title} />
-                    ))}
-                </View>
-            </ScrollView>
-            <View style={tw`content-end p-4`}>
-                <Buttons title="Другое" backgroundColor="white" textColor="red" onPress={openModal} />
-                <View style={tw`mt-2 content-end`}>
-                    <Buttons title="Сохранить" isDisebled={false} onPress={handleSave} />
-                </View>
-                <CenteredModal
-                    isModal={modalVisible}
-                    btnWhiteText='Добавить'
-                    btnRedText='Закрыть'
-                    isFullBtn={true}
-                    toggleModal={closeModal}
-                    
-                >
-                    <View style={tw`p-4 text-center`}>
-                        <Text style={tw`text-white text-xl mb-2 w-full`}>Добавьте свою специализацию</Text>
-                        <TextInput
-                            style={tw`bg-white p-3 rounded-xl text-lg text-black`}
-                            multiline
-                            numberOfLines={4}
-                            placeholder="Введите текст"
-                            value={textAreaValue}
-                            onChangeText={setTextAreaValue}
-                            scrollEnabled={true}
-                        />
-                    </View>
-                </CenteredModal>
+const handleAdd = () => {
+    handleSave();
+};
+return (
+    <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
+            <View style={tw`w-full p-3`}>
+                <NavigationMenu name="Специализация" />
+                {services.length !== 0 && services.map((service, index) => (
+                    <ServicesCategory key={index} title={service.title} />
+                ))}
             </View>
-        </SafeAreaView>
-    );
+        </ScrollView>
+        <View style={tw`content-end p-4`}>
+            <Buttons title="Другое" backgroundColor="white" textColor="red" onPress={openModal} />
+            <View style={tw`mt-2 content-end`}>
+            <Buttons title="Сохранить" onPress={() => router.push('/serviceStyle')} />
+            </View>
+            <CenteredModal
+                isModal={modalVisible}
+                btnWhiteText='Закрыть'
+                btnRedText='Добавить '
+                isFullBtn={true}
+                toggleModal={closeModal}
+                onConfirm={handleAdd}
+               
+            >
+                <View style={tw`p-4 text-center`}>
+                    <Text style={tw`text-white text-xl mb-2 w-full`}>Добавьте свою специализацию</Text>
+                    <TextInput
+                        style={tw`bg-white p-3 rounded-xl text-lg text-black`}
+                        multiline
+                        numberOfLines={4}
+                        placeholder="Введите текст"
+                        value={textAreaValue}
+                        onChangeText={setTextAreaValue}
+                        scrollEnabled={true}
+                    />
+                </View>
+            </CenteredModal>
+        </View>
+    </SafeAreaView>
+);
 };
 
 export default Expertise;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,6 +27,7 @@ import Canceledentries from './(profile)/(sessionhistory)/components/Canceledent
 import Settings from './(profile)/(settings)';
 import { StompProvider, } from '@/context/StompContext';
 import Expertise from './(standart)/(services)/(expertise)/expertise';
+import ServiceStyle from './(standart)/(services)/serviceStyle/serviceStyle';
 const Stack = createNativeStackNavigator();
 
 export {
@@ -88,6 +89,7 @@ function RootLayoutNav() {
           <Stack.Screen name="(standart)/(services)/(gender)/servesGender" component={ServesGender} options={{ headerShown: false }} />
           <Stack.Screen name="(standart)/(services)/(category)/category" component={Category} options={{ headerShown: false }} />
           <Stack.Screen name="(standart)/(services)/(expertise)/expertise" component={Expertise} options={{ headerShown: false }} />
+          <Stack.Screen name="(standart)/(services)/serviceStyle/serviceStyle" component={ServiceStyle} options={{ headerShown: false }} />
 
           {/*  */}
           <Stack.Screen name="(profile)/(sessionhistory)/sessionHistory" component={SessionHistory} options={{ title: 'Отменённые записи' }} />
