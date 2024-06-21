e4f3bfde65cbd2d4098e7b5186c623c1f4fbbd9e
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-06-21 15:57:46 +0300

    asdasda

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,26 @@
 import Buttons from "@/components/(buttons)/button";
-import {NavigationProp, useNavigation} from "@react-navigation/native";
-import {RootStackParamList} from "@/type/root";
-import {SafeAreaView} from "react-native-safe-area-context";
-import {StatusBar} from "react-native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "@/type/root";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 type SettingsScreenNavigationProp = NavigationProp<RootStackParamList, 'settings-locations-main'>;
 
 function Index() {
-    const navigation = useNavigation<SettingsScreenNavigationProp>();
-    return (
-        <>
-            <SafeAreaView style={[tw`flex-1 items-center justify-center px-5`, {backgroundColor: '#21212E'}]}>
-                <StatusBar barStyle="light-content" backgroundColor={`#21212E`}/>
-                <Buttons title={`Go Glavniy page`} onPress={() => navigation.navigate('(tabs)')}/>
-            </SafeAreaView>
-        </>
-    )
+  const navigation = useNavigation<SettingsScreenNavigationProp>();
+  return (
+    <>
+      <SafeAreaView style={[tw`flex-1 items-center justify-center px-5`, { backgroundColor: '#21212E' }]}>
+        <StatusBar barStyle="light-content" backgroundColor={`#21212E`} />
+        <Buttons title={`Go Glavniy page`} onPress={() => navigation.navigate('(tabs)')} />
+        <View style={tw`mb-5`}>
+
+        </View>
+        <Buttons title={`Go Login page`} onPress={() => navigation.navigate('(auth)/auth')} />
+      </SafeAreaView>
+    </>
+  )
 }
 
 export default Index;
