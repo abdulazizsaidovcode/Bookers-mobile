61e814865e4e32a4dab83cd15729c5406b681527
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-06-22 13:20:36 +0500

    er

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,6 @@ import { RootStackParamList } from "@/type/root";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
-import { router } from "expo-router";
 
 
 type SettingsScreenNavigationProp = NavigationProp<RootStackParamList, 'settings-locations-main'>;
@@ -16,12 +15,13 @@ function Index() {
     <>
       <SafeAreaView style={[tw`flex-1 items-center justify-center px-5`, { backgroundColor: '#21212E' }]}>
         <StatusBar barStyle="light-content" backgroundColor={`#21212E`} />
-        <Buttons title={`Go Glavniy page`} onPress={() => router.push('(free)/(work-grafic)/workMain')} />
+        <Buttons title={`Go Glavniy page`} onPress={() => navigation.navigate('(tabs)')} />
         <View style={tw`mb-5`}>
 
         </View>
         <Buttons title={`Go Login page`} onPress={() => navigation.navigate('(auth)/auth')} />
       </SafeAreaView>
+      
     </>
   )
 }
