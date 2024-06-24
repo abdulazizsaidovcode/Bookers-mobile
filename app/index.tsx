c27f32d7313d9bde46e0e850d8177e49242c506c
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-06-24 16:52:38 +0500

    nmadur uzgardida karochi

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,23 @@
-
+import React from 'react';
 import Buttons from "@/components/(buttons)/button";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { RootStackParamList } from "@/type/root";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { StatusBar, View } from "react-native";
+import {NavigationProp, useNavigation} from "@react-navigation/native";
+import {RootStackParamList} from "@/type/root";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {StatusBar, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 
 type SettingsScreenNavigationProp = NavigationProp<RootStackParamList, 'settings-locations-main'>;
 
-function Index() {
-  const navigation = useNavigation<SettingsScreenNavigationProp>();
-  return (
-    <>
-      <SafeAreaView style={[tw`flex-1 items-center justify-center px-5`, { backgroundColor: '#21212E' }]}>
-        <StatusBar barStyle="light-content" backgroundColor={`#21212E`} />
-        <Buttons title={`Go    page`} onPress={() => navigation.navigate('(tabs)')} />
-        <View style={tw`mb-5`}>
-
-        </View>
-        <Buttons title={`Go Login page`} onPress={() => navigation.navigate('(auth)/auth')} />
-      </SafeAreaView>
-      
-    </>
-  )
+const Index: React.FC = () => {
+    const navigation = useNavigation<SettingsScreenNavigationProp>();
+    return (
+        <SafeAreaView style={[tw`flex-1 items-center justify-center px-5`, {backgroundColor: '#21212E'}]}>
+            <StatusBar barStyle="light-content" backgroundColor={`#21212E`}/>
+            <Buttons title={`Go page`} onPress={() => navigation.navigate('(tabs)')}/>
+            <View style={tw`mb-5`}></View>
+            <Buttons title={`Go Login page`} onPress={() => navigation.navigate('(auth)/auth')}/>
+        </SafeAreaView>
+    )
 }
 export default Index;
