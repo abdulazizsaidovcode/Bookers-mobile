778dac5d7cb5d3aed8d07cfbeea46e6082a2648b
MingboyevAlisher2009 <mingboyevalisher171@gmail.com>
2024-06-24 15:28:15 +0500

    token updated

--- helpers/token.tsx
+++ helpers/token.tsx
@@ -1,7 +1,10 @@
 export const config = {
   headers: {
-    Authorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTA4NzE0MDEwIn0.DrCLuYvE8__dTgCmR62686SA_FbUvb_MZsrqdu1-EdxomxtxcLYchexWI-8UjkQqHWUxvL_OImHXuiR-ZKAz3w"
-  }
+    Authorization:
+      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og",
+  },
 };
 
-export const setConfig = (): string | null => config.headers.Authorization = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTA4NzE0MDEwIn0.DrCLuYvE8__dTgCmR62686SA_FbUvb_MZsrqdu1-EdxomxtxcLYchexWI-8UjkQqHWUxvL_OImHXuiR-ZKAz3w"
+export const setConfig = (): string | null =>
+  (config.headers.Authorization =
+    "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og");
