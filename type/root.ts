51fec7c45d02fdc8c2fcb3b3a2ee7c7a5830375c
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-06-26 10:49:39 +0500

    root update

--- type/root.ts
+++ type/root.ts
@@ -15,7 +15,7 @@ export type RootStackParamList = {
     '(free)/(client)/main': undefined | string;
     '(free)/(client)/client-list': undefined | string;
     '(free)/(client)/address-book': undefined | string;
-    '(free)/(client)/creating-client': { client: ClientAddressBook };
+    '(free)/(client)/creating-client': undefined | string;
     '(free)/(client)/updating-address-book': { client: ClientAddressBook };
     '(tabs)': undefined | string;
     '(welcome)/Welcome': undefined | string;
