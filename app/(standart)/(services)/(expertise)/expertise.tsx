50ee0bbeeb2d8b0cd07e7d95cd320624a42065ef
omadbekolimov <omadbekolimov96@gmail.com>
2024-06-28 20:03:15 +0500

    goo

--- app/(standart)/(services)/(expertise)/expertise.tsx
+++ app/(standart)/(services)/(expertise)/expertise.tsx
@@ -1,37 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, View, Text, TextInput, StatusBar, FlatList, TouchableOpacity } from 'react-native';
+import { ScrollView, View, Text, StatusBar, FlatList, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
 import NavigationMenu from '@/components/navigation/navigation-menu';
 import ServicesCategory from '@/components/services/servicesCatgegory';
 import Buttons from '@/components/(buttons)/button';
 import CenteredModal from '@/components/(modals)/modal-centered';
-import { router, useNavigation } from 'expo-router';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 import servicesStore from '@/helpers/state_managment/services/servicesStore';
 import axios from 'axios';
-import { base_url, category_child, masterAdd_category } from '@/helpers/api';
+import { category_child, masterAdd_category } from '@/helpers/api';
 import { config } from '@/helpers/token';
 import { useRoute } from '@react-navigation/native';
 import Textarea from '@/components/select/textarea';
-import { ActivityIndicator } from 'react-native-paper';
-import { RootStackParamList } from '@/type/root';
-import Process from '../(process)/process';
 
 const Expertise: React.FC = () => {
     const route = useRoute();
     const { childCategoryData, categoryFatherId, setChildCategoryData } = servicesStore();
     const [modalVisible, setModalVisible] = useState<boolean>(false);
     const [value, setValue] = useState('');
+    const [validate, setValidate] = useState(false);
     const [selectedCategories, setSelectedCategories] = useState<any[]>([]);
     const [selectedServices, setSelectedServices] = useState<any[]>([]);
     const { id } = route.params as { id: string };
+
     useEffect(() => {
         if (categoryFatherId && categoryFatherId.key) {
             postCategory(categoryFatherId.key, '');
         }
     }, []);
 
+    useEffect(() =>{
+        if(value.trim() === ""){
+            setValidate(false);
+        }
+        else{
+            setValidate(true);
+        }
+    },  [value]);
 
     const getChildCategory = async (id: string) => {
         try {
@@ -44,7 +50,6 @@ const Expertise: React.FC = () => {
                         name: item.name,
                     }));
                 setChildCategoryData(child);
-
             } else {
                 setChildCategoryData([]);
             }
@@ -70,13 +75,18 @@ const Expertise: React.FC = () => {
             console.error("Error fetching child categories:", error);
         }
     };
+
     const openModal = () => setModalVisible(true);
     const closeModal = () => setModalVisible(false);
+
     const handleAdd = () => {
-        postCategory(id, value);
-        closeModal();
-        setValue("")
+        if (value.trim() !== "") {
+            postCategory(id, value);
+            closeModal();
+            setValue("");
+        }
     };
+
     const handleSelect = (item: any) => {
         setSelectedServices((prevSelected) => {
             if (prevSelected.find((service: any) => service.name === item.name)) {
@@ -86,6 +96,7 @@ const Expertise: React.FC = () => {
             }
         });
     };
+
     const renderItem = ({ item }: { item: any }) => {
         const isSelected = selectedServices.find((service: any) => service.name === item.name);
         return (
@@ -98,6 +109,7 @@ const Expertise: React.FC = () => {
             </TouchableOpacity>
         );
     };
+
     return (
         <SafeAreaView style={[tw`flex-1`, { backgroundColor: '#21212E' }]}>
             <StatusBar backgroundColor={`#21212E`} barStyle={`light-content`} />
@@ -105,12 +117,12 @@ const Expertise: React.FC = () => {
             <View style={[tw`flex-1`, { backgroundColor: '#21212E' }]}>
                 <ScrollView
                     showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{ paddingHorizontal: 16, flexGrow: 1, justifyContent: 'space-between', backgroundColor: '#21212E' }}  >
-                   <View style={tw`w-full`}>
+                    contentContainerStyle={{ paddingHorizontal: 16, flexGrow: 1, justifyContent: 'space-between', backgroundColor: '#21212E' }}>
+                    <View style={tw`w-full`}>
                         <FlatList
                             data={childCategoryData}
                             renderItem={renderItem}
-                            keyExtractor={(item, index) => index.toString()}/>
+                            keyExtractor={(item, index) => index.toString()} />
                     </View>
                     <View style={tw`content-end mb-3`}>
                         <Buttons title="Другое" backgroundColor="white" textColor="red" onPress={openModal} />
@@ -118,9 +130,8 @@ const Expertise: React.FC = () => {
                             <Buttons
                                 title="Сохранить"
                                 onPress={() => router.push('../(process)/process')}
-                            // isDisebled={selectedCategories.length!==0}
+                                // disabled={selectedCategories.length === 0}
                             />
-                            
                         </View>
                         <CenteredModal
                             isModal={modalVisible}
@@ -128,7 +139,8 @@ const Expertise: React.FC = () => {
                             btnRedText='Закрыть '
                             isFullBtn={false}
                             toggleModal={closeModal}
-                            onConfirm={handleAdd}>
+                            onConfirm={handleAdd}
+                            disabled={validate}>
                             <View style={tw`p-4 text-center`}>
                                 <Text style={tw`text-white text-xl mb-2 w-full`}>Добавьте свою специализацию</Text>
                                 <Textarea
@@ -143,4 +155,5 @@ const Expertise: React.FC = () => {
         </SafeAreaView>
     );
 };
+
 export default Expertise;
