7a8308a631045f365ce4d27f3e320c1e4d603a38
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-06-28 15:21:58 +0500

    kalendar va bazi book qilinadigan qisimlar boldi

--- app/(free)/(work-grafic)/calendar.tsx
+++ app/(free)/(work-grafic)/calendar.tsx
@@ -1,10 +1,11 @@
 import graficWorkStore from "@/helpers/state_managment/graficWork/graficWorkStore";
 import { DateObject } from "@/type/graficWork/graficWork";
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Dimensions, SafeAreaView, StyleSheet } from "react-native";
 import { Calendar } from "react-native-calendars";
 import { MarkedDates } from "react-native-calendars/src/types";
-import tw from "tailwind-react-native-classnames";
+
+const {width, height} = Dimensions.get("window");
 
 const CalendarGraffic: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<MarkedDates>({});
@@ -16,7 +17,7 @@ const CalendarGraffic: React.FC = () => {
         selected: true,
         marked: true,
         dotColor: "red",
-        color: "#9C0A35",
+        selectedColor: "#9C0A35",
       },
     };
 
@@ -27,9 +28,31 @@ const CalendarGraffic: React.FC = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Calendar
-        style={[tw`w-80`]} // width va height ni tailwind orqali beramiz
+        style={styles.calendar} // Calendar uslubi uchun style prop qo'shildi
         onDayPress={onDayPress}
         markedDates={selectedDate}
+        theme={{
+          backgroundColor: "#ffffff",
+          calendarBackground: "#ffffff",
+          textSectionTitleColor: "#b6c1cd",
+          selectedDayBackgroundColor: "#9C0A35",
+          selectedDayTextColor: "#ffffff",
+          todayTextColor: "#00adf5",
+          dayTextColor: "#2d4150",
+          textDisabledColor: "#d9e1e8",
+          monthTextColor: "#2d4150",
+          arrowColor: "#9C0A35",
+          textDayFontFamily: "monospace",
+          textMonthFontFamily: "monospace",
+          textDayHeaderFontFamily: "monospace",
+          textInactiveColor: "#9C0A35",
+          textDayFontWeight: "300",
+          textMonthFontWeight: "bold",
+          textDayHeaderFontWeight: "300",
+          textDayFontSize: 16,
+          textMonthFontSize: 18,
+          textDayHeaderFontSize: 16,
+        }}
       />
     </SafeAreaView>
   );
@@ -38,11 +61,20 @@ const CalendarGraffic: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    width: "100%",
+    backgroundColor: "#2A2A2A",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 30,
-    marginHorizontal: 12,
+    borderRadius: 20,
+    height: "auto",
+
+    padding: 20,
+  },
+  calendar: {
+    width: width / 1.1,
+    height: "auto",
+    borderRadius: 20,
+    paddingBottom: 10,
   },
 });
 
--- helpers/api-function/freeTime/freeTime.tsx
+++ helpers/api-function/freeTime/freeTime.tsx
@@ -4,13 +4,12 @@ import axios from "axios";
 
 export function getFreeTime(date: string | null, setData: (val: any) => void) {
     const url = `${free_time_list}?date=${date}&masterId=0e84b065-cf7e-4799-b0a3-43b3233d0ae6`;
-    
-    axios
-        .get(url, config)
+
+    axios.get(url, config)
         .then((res) => {
             setData(res.data.body);
         })
-        .catch((err) => {
+        .catch(() => {
             setData([]);
         });
 }
