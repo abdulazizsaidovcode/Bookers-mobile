1c543cf63ccf9657788ed26b388c148fa2b37776
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-07-02 10:34:58 +0500

    api edit yulab olela

--- app/(free)/(client)/details/history/past-history.tsx
+++ app/(free)/(client)/details/history/past-history.tsx
@@ -1,12 +1,13 @@
 import AppointmentCard from "@/components/(cards)/appointment-card";
 import {RootStackParamList} from "@/type/root";
-import {NavigationProp, useNavigation} from "@react-navigation/native";
+import {NavigationProp, RouteProp, useNavigation, useRoute} from "@react-navigation/native";
 import NavigationMenu from "@/components/navigation/navigation-menu";
 import tw from "tailwind-react-native-classnames";
 import {ScrollView, StatusBar, View} from "react-native";
 import React from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
 
+type CreatingClientScreenRouteProp = RouteProp<RootStackParamList, '(free)/(client)/details/history/past-history'>;
 type SettingsScreenNavigationProp = NavigationProp<RootStackParamList, '(free)/(client)/details/history/past-history'>;
 
 const data = [
@@ -22,6 +23,8 @@ const data = [
 
 const PastHistory = () => {
     const navigation = useNavigation<SettingsScreenNavigationProp>();
+    const route = useRoute<CreatingClientScreenRouteProp>();
+    const {clientID} = route.params;
     return (
         <SafeAreaView style={[tw`flex-1`, {backgroundColor: '#21212E'}]}>
             <StatusBar backgroundColor={`#21212E`} barStyle={`light-content`}/>
