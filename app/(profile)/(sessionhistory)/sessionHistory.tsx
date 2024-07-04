890761f03a603e04a87fd013590d70b1a662956b
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-07-04 11:21:26 +0500

    sessionn bazi ozgarishlar kiritildi tushunmagan bolsiela pochdta  yoziyla

--- app/(profile)/(sessionhistory)/sessionHistory.tsx
+++ app/(profile)/(sessionhistory)/sessionHistory.tsx
@@ -1,6 +1,6 @@
-   import React from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList, SafeAreaView } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import NavigationMenu from '@/components/navigation/navigation-menu';
 
@@ -9,9 +9,9 @@ const sessionData = [
     { id: '2', title: 'Прошедшие записи', icon: 'history', count: 2, screen: '(profile)/(sessionhistory)/components/Pastentries/Pastentries' },
     { id: '3', title: 'Отменённые записи', icon: 'times-circle', count: 1, screen: '(profile)/(sessionhistory)/components/Canceledentries/Canceledentries' },
 ];
-                    
+
 const SessionHistory: React.FC = () => {
-    const navigation = useNavigation();            
+    const navigation = useNavigation();
 
     const renderItem = ({ item }: any) => (
         <TouchableOpacity
@@ -19,21 +19,20 @@ const SessionHistory: React.FC = () => {
             onPress={() => navigation.navigate(item.screen)}
         >
             <View style={styles.itemContent}>
-                <FontAwesome name={item.icon} size={24} color="#E74C3C" style={styles.itemIcon} />
+                <FontAwesome name={item.icon} size={24} color="#9c0935" style={styles.itemIcon} />
                 <Text style={styles.itemText}>{item.title}</Text>
             </View>
             <View style={styles.itemContent}>
                 <Text style={styles.itemCount}>{item.count}</Text>
-                <FontAwesome name="chevron-right" size={24} color="#E74C3C" style={styles.itemIcon} />
+                <MaterialIcons name="navigate-next" size={36} color='gray' />
             </View>
         </TouchableOpacity>
     );
 
     return (
         <SafeAreaView style={styles.container}>
-            <NavigationMenu name='Tarifi' />
+            <NavigationMenu name='История сеансов' />
             <View style={styles.padding}>
-                <Text style={styles.title}>История сеансов</Text>
                 <FlatList
                     data={sessionData}
                     renderItem={renderItem}
@@ -80,10 +79,15 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     itemCount: {
-        color: 'black',
+        backgroundColor: '#9c0935',
+        color: '#fff',
+        paddingHorizontal: 5,
+        paddingVertical: 1,
+        width: 20,
         fontSize: 16,
         fontWeight: 'bold',
         marginRight: 16,
+        
     },
 });
 
