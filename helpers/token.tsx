4a59d0e42e63420ed98e54fc760d0df3937bbe2f
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-07-04 09:31:02 +0500

    sckdhivus

--- helpers/token.tsx
+++ helpers/token.tsx
@@ -1,42 +1,23 @@
 import axios from "axios";
 import { getMe } from "./api";
 
-// export const config = {
-//   headers: {
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og",
-//   },
-// };
-
 export const config = {
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTA4OTA0ODExIn0.HMmXsybX0YzxBKUm8It7cd3msVThLw0M3yN_AO-kv7CRnNxpF6Ft12s-Wmi_E4KrX6qIKU440lCvTuNh5Gz40w",
+      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og",
   },
 };
-
-// export const imageConfig = {
-//   headers: {
-//     'Content-Type': 'multipart/form-data',
-//     Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og`
-//   }
-// }
 
 export const imageConfig = {
   headers: {
     'Content-Type': 'multipart/form-data',
-    Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTA4OTA0ODExIn0.HMmXsybX0YzxBKUm8It7cd3msVThLw0M3yN_AO-kv7CRnNxpF6Ft12s-Wmi_E4KrX6qIKU440lCvTuNh5Gz40w`
+    Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og`
   }
 }
 
-// export const setConfig = (): string | null =>
-//   (config.headers.Authorization =
-//     "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og");
-
 export const setConfig = (): string | null =>
   (config.headers.Authorization =
-    "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTA4OTA0ODExIn0.HMmXsybX0YzxBKUm8It7cd3msVThLw0M3yN_AO-kv7CRnNxpF6Ft12s-Wmi_E4KrX6qIKU440lCvTuNh5Gz40w");
-
+    "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og");
 
 export const getMee = ( setData: (val: any) => void ) => {
   axios.get(getMe, config)
