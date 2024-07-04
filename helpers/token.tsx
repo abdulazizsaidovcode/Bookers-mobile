73ffabefcf729938f3281d48f53616bf43bbf0a5
omadbekolimov <omadbekolimov96@gmail.com>
2024-07-04 10:18:16 +0500

    api o'zgardi

--- helpers/token.tsx
+++ helpers/token.tsx
@@ -1,10 +1,18 @@
 import axios from "axios";
 import { getMe } from "./api";
 
+// export const config = {
+//   headers: {
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og",
+//   },
+// };
+
+// local hostda ishlash uchun 
 export const config = {
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTAwMDQzMDUxIn0.K-7YCoydy9DsZOkdan4QTWcnrWnFhSWRhqSXqa_PrxfOx4K_VCffMjfdejuZKSuWF4055eHy1m3Y81qxYfg2og",
+      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrOTk4OTA4OTA0ODExIn0.HMmXsybX0YzxBKUm8It7cd3msVThLw0M3yN_AO-kv7CRnNxpF6Ft12s-Wmi_E4KrX6qIKU440lCvTuNh5Gz40w",
   },
 };
 
