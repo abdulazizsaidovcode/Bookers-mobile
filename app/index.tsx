034f7e2ff3c5c9331de76a8391e17fddda38a831
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-07-06 18:16:57 +0500

    index update

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import TabLayout from './(tabs)/_layout';
-import RemindAboutAppointment from './(notifications)/(pages)/remind-about-appointment';
-import CancelRecording from './(notifications)/(pages)/cancel-recording';
 
 const Index: React.FC = () => {
     return (
-        <CancelRecording />
+        <TabLayout />
     )
 }
 export default Index;
