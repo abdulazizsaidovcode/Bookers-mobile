c61bf29bb67080d59e6f28a96a39ffcc1a752f83
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-07-06 17:59:17 +0500

    update

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,7 @@ import TabLayout from './(tabs)/_layout';
 
 const Index: React.FC = () => {
     return (
-<<<<<<< HEAD
-        <RemindAboutAppointment />
-        //bu joyga hamma ham yozurib zerikib ketmadimi margeni olurib 
-=======
-       <TabLayout/>
->>>>>>> af798d0d80e3f63bdbee18de6ae2d2d4b1a0d32a
+        <TabLayout />
     )
 }
 export default Index;
