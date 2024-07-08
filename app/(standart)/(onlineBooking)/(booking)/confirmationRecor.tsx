fd26106d6b2b06005c1781df9db55ae806289674
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-08 12:08:12 +0300

    ddd

---
+++ app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ConfirmationRecord = () => {
+    return (
+        <View>
+            <Text>ConfirmationRecord</Text>
+        </View>
+    )
+}
+
+export default ConfirmationRecord
+
+const styles = StyleSheet.create({})
