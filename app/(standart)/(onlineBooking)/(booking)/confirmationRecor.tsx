d45c5def479a509ea754dc2c9dac40fe3bf4f249
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-08 14:49:43 +0300

    api uladndi

--- app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
+++ app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
@@ -5,6 +5,9 @@ import NavigationMenu from '@/components/navigation/navigation-menu'
 import SwitchWithLabel from '@/components/switchWithLabel/switchWithLabel'
 import SwitchWithLabelBlack from '@/components/switchWithLabel/switchWithLabelBlack'
 import { OnlineBookingStory } from '@/helpers/state_managment/onlinBooking/onlineBooking'
+import Buttons from '@/components/(buttons)/button'
+import { onlineConfirmationServices } from '@/helpers/api-function/onlineBooking/onlineBooking'
+import { router } from 'expo-router'
 
 const ConfirmationRecord = () => {
 
@@ -34,35 +37,43 @@ const ConfirmationRecord = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={{ marginBottom: 10, }}></Text>
-            <NavigationMenu name={`Онлайн бронирование`} />
-            <StatusBar style="auto" />
-            <View style={{ paddingHorizontal: 16, marginBottom: 10, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
-                <SwitchWithLabelBlack
-                    label="Подтверждать записи для всех клиентов"
-                    value={isEnabled}
-                    onToggle={toggleSwitch} />
-            </View>
-            <View style={{ paddingHorizontal: 16, marginBottom: 10, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
-                <SwitchWithLabelBlack
-                    label="Подтверждать записи только 
-                    для новых клиентов"
-                    value={isEnabled2}
-                    onToggle={toggleSwitch2} />
-            </View>
-            <View style={{ paddingHorizontal: 16, marginBottom: 20, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
-                <SwitchWithLabelBlack
-                    label="Не подтверждать записи"
-                    value={isEnabled3}
-                    onToggle={toggleSwitch3} />
-            </View>
+            <View>
 
-            <Text style={{ marginBottom: 10, color: 'white' }}>
-                Настройте подтверждение записи Вы можете подтверждать каждую запись и приложение будет отправлять увеедомления клиентам
-            </Text>
-            <Text style={{ marginBottom: 10, color: 'white' }}>
-                Или клиенты будут бронировать Ваши услуги без Вашего подтверждения и Вы будете видеть всех записанных клиентов
-            </Text>
+                <Text style={{ marginBottom: 10, }}></Text>
+                <NavigationMenu name={`Онлайн бронирование`} />
+                <StatusBar style="auto" />
+                <View style={{ paddingHorizontal: 16, marginBottom: 10, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
+                    <SwitchWithLabelBlack
+                        label="Подтверждать записи для всех клиентов"
+                        value={isEnabled}
+                        onToggle={toggleSwitch} />
+                </View>
+                <View style={{ paddingHorizontal: 16, marginBottom: 10, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
+                    <SwitchWithLabelBlack
+                        label="Подтверждать записи только 
+                    для новых клиентов"
+                        value={isEnabled2}
+                        onToggle={toggleSwitch2} />
+                </View>
+                <View style={{ paddingHorizontal: 16, marginBottom: 20, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
+                    <SwitchWithLabelBlack
+                        label="Не подтверждать записи"
+                        value={isEnabled3}
+                        onToggle={toggleSwitch3} />
+                </View>
+
+                <Text style={{ marginBottom: 10, color: 'white' }}>
+                    Настройте подтверждение записи Вы можете подтверждать каждую запись и приложение будет отправлять увеедомления клиентам
+                </Text>
+                <Text style={{ marginBottom: 10, color: 'white' }}>
+                    Или клиенты будут бронировать Ваши услуги без Вашего подтверждения и Вы будете видеть всех записанных клиентов
+                </Text>
+
+            </View>
+            <Buttons title='Save' backgroundColor='#9C0A35' onPress={() => {
+                onlineConfirmationServices(isEnabled, isEnabled2, isEnabled3)
+                router.push('(standart)/(onlineBooking)/onlineBooking')
+            }} />
         </SafeAreaView>
     )
 }
@@ -72,6 +83,7 @@ export default ConfirmationRecord
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'space-between',
         backgroundColor: '#21212E',
         padding: 16,
     },
