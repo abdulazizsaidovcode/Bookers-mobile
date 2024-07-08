6e79cb45767384bbaeedb692559c332e8d8fc425
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-08 12:51:13 +0300

    boldim deb uylashga harakat qilaman

--- app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
+++ app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
@@ -1,13 +1,50 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import NavigationMenu from '@/components/navigation/navigation-menu'
+import SwitchWithLabel from '@/components/switchWithLabel/switchWithLabel'
+import SwitchWithLabelBlack from '@/components/switchWithLabel/switchWithLabelBlack'
 
 const ConfirmationRecord = () => {
+    const [isEnabled, setIsEnabled] = useState(false);
+
+    const toggleSwitch = () => {
+        const newValue = !isEnabled;
+        setIsEnabled(newValue);
+        // setAllowClient(newValue); // Update the global state
+        // onlineBookingAllowClient(newValue)
+        console.log(newValue);
+    };
     return (
         <SafeAreaView style={styles.container}>
             <Text style={{ marginBottom: 10, }}></Text>
             <NavigationMenu name={`Онлайн бронирование`} />
+            <StatusBar style="auto" />
+            <View style={{ paddingHorizontal: 16, marginBottom: 10, backgroundColor: 'white', borderRadius: 15 }}>
+                <SwitchWithLabelBlack
+                    label="Подтверждать записи для всех клиентов"
+                    value={isEnabled}
+                    onToggle={toggleSwitch} />
+            </View>
+            <View style={{ paddingHorizontal: 16, marginBottom: 10, backgroundColor: 'white', borderRadius: 15 }}>
+                <SwitchWithLabelBlack
+                    label="Подтверждать записи только для новых клиентов"
+                    value={isEnabled}
+                    onToggle={toggleSwitch} />
+            </View>
+            <View style={{ paddingHorizontal: 16, marginBottom: 20, backgroundColor: 'white', borderRadius: 15 }}>
+                <SwitchWithLabelBlack
+                    label="Не подтверждать записи"
+                    value={isEnabled}
+                    onToggle={toggleSwitch} />
+            </View>
+
+            <Text style={{ marginBottom: 10, color: 'white' }}>
+                Настройте подтверждение записи Вы можете подтверждать каждую запись и приложение будет отправлять увеедомления клиентам
+            </Text>
+            <Text style={{ marginBottom: 10, color: 'white' }}>
+                Или клиенты будут бронировать Ваши услуги без Вашего подтверждения и Вы будете видеть всех записанных клиентов
+            </Text>
         </SafeAreaView>
     )
 }
@@ -18,5 +55,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#21212E',
+        padding: 16,
     },
 })
