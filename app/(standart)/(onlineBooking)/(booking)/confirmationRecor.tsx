055b889a642ac3ee5ab2116df0bb72dc628f77f5
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-08 12:13:10 +0300

    Good job

--- app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
+++ app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const ConfirmationRecord = () => {
     return (
-        <View>
-            <Text>ConfirmationRecord</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text></Text>
+        </SafeAreaView>
     )
 }
 
 export default ConfirmationRecord
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#21212E',
+    },
+})
