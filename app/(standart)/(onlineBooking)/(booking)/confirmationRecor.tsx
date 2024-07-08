9041c5ff4b8696e766b7e2944bd070b05c603573
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-08 13:41:08 +0300

    confirma qisimi tugadi

--- app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
+++ app/(standart)/(onlineBooking)/(booking)/confirmationRecor.tsx
@@ -8,10 +8,24 @@ import { OnlineBookingStory } from '@/helpers/state_managment/onlinBooking/onlin
 
 const ConfirmationRecord = () => {
 
-    const { isEnabled, setIsEnabled } = OnlineBookingStory();
+    const { isEnabled, setIsEnabled, isEnabled2, setIsEnabled2, isEnabled3, setIsEnabled3 } = OnlineBookingStory();
     const toggleSwitch = () => {
         const newValue = !isEnabled;
         setIsEnabled(newValue);
+        // setAllowClient(newValue); // Update the global state
+        // onlineBookingAllowClient(newValue)
+        console.log(newValue);
+    };
+    const toggleSwitch2 = () => {
+        const newValue = !isEnabled2;
+        setIsEnabled2(newValue);
+        // setAllowClient(newValue); // Update the global state
+        // onlineBookingAllowClient(newValue)
+        console.log(newValue);
+    };
+    const toggleSwitch3 = () => {
+        const newValue = !isEnabled3;
+        setIsEnabled3(newValue);
         // setAllowClient(newValue); // Update the global state
         // onlineBookingAllowClient(newValue)
         console.log(newValue);
@@ -31,14 +45,14 @@ const ConfirmationRecord = () => {
                 <SwitchWithLabelBlack
                     label="Подтверждать записи только 
                     для новых клиентов"
-                    value={isEnabled}
-                    onToggle={toggleSwitch} />
+                    value={isEnabled2}
+                    onToggle={toggleSwitch2} />
             </View>
             <View style={{ paddingHorizontal: 16, marginBottom: 20, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
                 <SwitchWithLabelBlack
                     label="Не подтверждать записи"
-                    value={isEnabled}
-                    onToggle={toggleSwitch} />
+                    value={isEnabled3}
+                    onToggle={toggleSwitch3} />
             </View>
 
             <Text style={{ marginBottom: 10, color: 'white' }}>
