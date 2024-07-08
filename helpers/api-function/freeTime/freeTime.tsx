95a482e0224a21f4dc2119ffec9a8733a0668b61
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-07-08 09:29:34 +0500

    salomlar

--- helpers/api-function/freeTime/freeTime.tsx
+++ helpers/api-function/freeTime/freeTime.tsx
@@ -1,11 +1,9 @@
-import { free_time_list } from "@/helpers/api";
-import { config } from "@/helpers/token";
+import {free_time_list} from "@/helpers/api";
+import {config} from "@/helpers/token";
 import axios from "axios";
 
-export function getFreeTime(date: string | null, setData: (val: any) => void) {
-    const url = `${free_time_list}?date=${date}&masterId=0e84b065-cf7e-4799-b0a3-43b3233d0ae6`;
-
-    axios.get(url, config)
+export function getFreeTime(date: string | null, setData: (val: any) => void, masterID?: string) {
+    axios.get(`${free_time_list}?date=${date}&masterId=${masterID ? masterID : 'accec2c2-a471-4da8-ad26-812b83d2b103'}`, config)
         .then((res) => {
             setData(res.data.body);
         })
