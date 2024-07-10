c9eef97c0ec0cf1c56edff037378fb8a1c535a00
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-10 11:17:34 +0300

    git add

---
+++ app/(standart)/(onlineBooking)/(booking)/requestWindow.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const requestWindow = () => {
+    return (
+        <View>
+            <Text>requestWindow</Text>
+        </View>
+    )
+}
+
+export default requestWindow
+
+const styles = StyleSheet.create({})
