1fc3c939d66d23aee8218adc15b5b09026dac2e0
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-10 12:43:26 +0300

    Hammasi yahshi buladi deb uylayman

--- app/(standart)/(onlineBooking)/(booking)/requestWindow.tsx
+++ app/(standart)/(onlineBooking)/(booking)/requestWindow.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const RequestWindow = () => {
     return (
         <View>
-            <Text>RequestWindow</Text>
+            <Text style={{ fontSize: 20 }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, hic! Saepe, sit praesentium illum cum eveniet dolorum quam harum dolorem consequuntur deserunt ea, pariatur quos error ratione, atque hic eaque animi sunt dignissimos? Ut nulla dolores suscipit deserunt rerum porro ex cupiditate sunt quibusdam. Laboriosam temporibus explicabo consectetur laborum fugiat.</Text>
         </View>
     )
 }
