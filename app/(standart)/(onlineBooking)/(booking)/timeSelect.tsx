595f3afadacb00f928abb9c461ad4c742bb3a332
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-10 13:37:02 +0300

    hammasi uhshaydi degan edimku

--- app/(standart)/(onlineBooking)/(booking)/timeSelect.tsx
+++ app/(standart)/(onlineBooking)/(booking)/timeSelect.tsx
@@ -4,20 +4,31 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import NavigationMenu from '@/components/navigation/navigation-menu'
 import SwitchWithLabelBlack from '@/components/switchWithLabel/switchWithLabelBlack'
 import Buttons from '@/components/(buttons)/button'
+import { OnlineBookingStory3 } from '@/helpers/state_managment/onlinBooking/onlineBooking'
 
 const TimeSelect = () => {
+    const { setTimeEnabled, timeEnabled } = OnlineBookingStory3()
+
+    const timeSwitch = () => {
+        const newValue = !timeEnabled
+        setTimeEnabled(newValue)
+    }
     return (
         <SafeAreaView style={styles.container}>
             <View>
                 <NavigationMenu name={`Онлайн бронирование`} />
-                <View style={{ paddingHorizontal: 16, marginBottom: 10, backgroundColor: '#B9B9C9', borderRadius: 15 }}>
+                <View style={styles.switchContainer}>
                     <SwitchWithLabelBlack
-                        onToggle={() => { }}
-                        value={true}
+                        onToggle={timeSwitch}
+                        value={timeEnabled}
                         label="Подтверждать записи для всех клиентов"
                     />
                 </View>
-                { }
+                {timeEnabled && (
+                    <View style={styles.messageContainer}>
+                        <Text style={styles.messageText}>Salom dunyo</Text>
+                    </View>
+                )}
             </View>
             <Buttons title="Сохранить" backgroundColor="#9C0A35" onPress={() => { }} />
         </SafeAreaView>
@@ -32,5 +43,21 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         backgroundColor: '#21212E',
         padding: 16,
+    },
+    switchContainer: {
+        paddingHorizontal: 16,
+        marginBottom: 10,
+        backgroundColor: '#B9B9C9',
+        borderRadius: 15,
+    },
+    messageContainer: {
+        padding: 16,
+        backgroundColor: '#B9B9C9',
+        borderRadius: 15,
+        marginTop: 10,
+    },
+    messageText: {
+        color: '#000',
+        fontSize: 16,
     },
 })
