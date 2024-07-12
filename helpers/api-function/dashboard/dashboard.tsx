2466a651a0c2739efdec90b24ecdce1b17a1a627
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-07-12 11:35:07 +0500

    dashboard update

--- helpers/api-function/dashboard/dashboard.tsx
+++ helpers/api-function/dashboard/dashboard.tsx
@@ -1,3 +1,4 @@
+import { getConfig } from "@/app/(tabs)/main";
 import { dashboard_daily_time_orders, dashboard_edit_order_status, dashboard_hall_order, dashboard_main_statistic, dashboard_today_work_grafic, dashboard_wait_order } from "@/helpers/api"
 import { config } from "@/helpers/token";
 import { DashboardDailyTimeOrders, DashboardHallingOrder, DashboardMainStatistic, DashboardWaitingOrder, TodayWorkGrafic } from "@/type/dashboard/dashboard";
@@ -6,6 +7,7 @@ import axios from "axios"
 
 export const fetchDaylyOrderTimes = async (setDailyTimeData: (val: DashboardDailyTimeOrders[]) => void, masterId: string) => {
     try {
+        const config = await getConfig()
         const { data } = await axios.get(`${dashboard_daily_time_orders}/${masterId}`, config);
         if (data.success) {
             setDailyTimeData(data.body);
@@ -15,6 +17,7 @@ export const fetchDaylyOrderTimes = async (setDailyTimeData: (val: DashboardDail
 
 export const fetchMainStatistic = async (setMainStatisticData: (val: DashboardMainStatistic) => void) => {
     try {
+        const config = await getConfig()
         const { data } = await axios.get(dashboard_main_statistic, config);
         if (data.success) {
             setMainStatisticData(data.body);
@@ -24,6 +27,7 @@ export const fetchMainStatistic = async (setMainStatisticData: (val: DashboardMa
 
 export const fetchWaitingOrders = async (setWaitingData: (val: DashboardHallingOrder[]) => void) => {
     try {
+        const config = await getConfig()
         const { data } = await axios.get(dashboard_wait_order, config);
         if (data.success) {
             setWaitingData(data.body);
@@ -33,6 +37,7 @@ export const fetchWaitingOrders = async (setWaitingData: (val: DashboardHallingO
 
 export const fetchHallingOrders = async (setHallData: (val: DashboardWaitingOrder[]) => void) => {
     try {
+        const config = await getConfig()
         const { data } = await axios.get(dashboard_hall_order, config);
         if (data.success) {
             setHallData(data.body);
@@ -42,6 +47,7 @@ export const fetchHallingOrders = async (setHallData: (val: DashboardWaitingOrde
 
 export const fetchTodayWorkGrafic = async (setTodayGrafic: (val: TodayWorkGrafic) => void, masterId: string) => {
     try {
+        const config = await getConfig()
         const { data } = await axios.get(`${dashboard_today_work_grafic}/${masterId}`, config);
         if (data.success) {
             setTodayGrafic(data.body);
@@ -51,10 +57,11 @@ export const fetchTodayWorkGrafic = async (setTodayGrafic: (val: TodayWorkGrafic
 
 export const editOrderStatus = async (setWaitingData: (val: DashboardWaitingOrder[]) => void, orderId: string, status: string, toggleModal: () => void) => {
     try {
+        const config = await getConfig()
         const { data } = await axios.put(`${dashboard_edit_order_status}?orderId=${orderId}&status=${status}`, {}, config);
         if (data.success) {
             fetchWaitingOrders(setWaitingData);
             toggleModal();
         }
-    } catch (error) { }
+    } catch { }
 }
