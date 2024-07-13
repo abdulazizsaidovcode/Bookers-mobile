665b08ee79fb1d2e63220713bcf65fdb6e88e9e7
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-07-13 19:46:12 +0500

    jjc

--- components/description/description.tsx
+++ components/description/description.tsx
@@ -10,13 +10,13 @@ interface DescriptionProps {
 
 const Description: React.FC<DescriptionProps> = ({ title, content, contactTitle, contactInfo }) => (
     <View style={tw`bg-gray-200 rounded-xl p-3`}>
-        <Text style={tw`font-bold text-xl text-center mb-5`}>
+        <Text style={tw`font-bold text-lg text-gray-800 text-center px-1 mb-5`}>
             {title}
         </Text>
-        <Text style={tw`font-sans mb-5`}>
+        <Text style={tw`font-sans mb-5 text-gray-900`}>
             {content}
         </Text>
-        <Text style={tw`text-center font-bold text-2xl`}>{contactTitle}</Text>
+        <Text style={tw`text-center text-gray-800 font-bold text-2xl`}>{contactTitle}</Text>
         <Text style={tw`text-center text-gray-600 mb-2`}>{contactInfo}</Text>
     </View>
 );
