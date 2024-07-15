1058619651ab606e9d5073b920ce22d986ebf517
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-15 16:00:51 +0300

    CheckPasswordni tug'irla

--- app/(checkPassword)/checkPassword.tsx
+++ app/(checkPassword)/checkPassword.tsx
@@ -119,7 +119,6 @@ const CheckPinOnCome: React.FC = () => {
                         ]}
                         onPress={() => {
                             handleContinue()
-
                         }}
                         disabled={!isButtonEnabled}
                     >
