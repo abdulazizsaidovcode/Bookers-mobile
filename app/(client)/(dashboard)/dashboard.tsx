6c62023317a1f53e4356e7b8d6394da2ed65bfe0
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-15 18:17:52 +0300

    bugancha  buladi endi

--- app/(client)/(dashboard)/dashboard.tsx
+++ app/(client)/(dashboard)/dashboard.tsx
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import Slider from '@react-native-community/slider';
-import CommunitySlider from '@/components/communiytSlider/communitySlider';
-import { FontAwesome } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const { width } = Dimensions.get('window');
+const  Tab= createBottomTabNavigator();
 
 const Dashboard = () => {
   const [value, setValue] = useState(1.5);
 
   return (
     <View style={{ flex: 1 , justifyContent: 'center', alignItems: 'center'}}>
-      <CommunitySlider
-        icon={<FontAwesome name="star" size={20} color="#8B1A1A" />}
-        title="+"
-      />
+      <Text>ffff</Text>
     </View>
   );
 };
