9f865c464227138f239dfe6e01dc2322055854e8
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-16 17:09:21 +0300

    yuqool

--- app/(client)/(dashboard)/(health)/health.tsx
+++ app/(client)/(dashboard)/(health)/health.tsx
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import NavigationMenu from '@/components/navigation/navigation-menu'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Health = () => {
   return (
-    <View>
-      <Text>Health</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 ,backgroundColor: '#21212E'}}>
+      <NavigationMenu name='Здоровье и красота волос' />
+      <ScrollView style={styles.container}>
+        <Text style={{ color: '#C2C2C2' }}>Подберите критерии услуг</Text>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default Health
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#21212E',
+    padding: 16,
+  },
+})
