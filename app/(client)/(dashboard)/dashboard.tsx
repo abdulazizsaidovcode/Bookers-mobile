b19af57274e647e72579d3b0397fb937e5fc1009
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-16 12:11:36 +0300

    ssss

--- app/(client)/(dashboard)/dashboard.tsx
+++ app/(client)/(dashboard)/dashboard.tsx
@@ -1,28 +1,27 @@
 import React, { useState } from 'react';
-import { View, Text,} from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView,} from 'react-native';
 import AccordionItem from './accardion';
 
 const Dashboard = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',padding:10 }}>
+    <SafeAreaView style={styles.container}>
+      
       <AccordionItem title="Accordion Item 1">
         <Text>This is the content of the first item.</Text>
       </AccordionItem>
       <AccordionItem title="Accordion Item 2">
         <Text>This is the content of the second item.</Text>
       </AccordionItem>
-      <AccordionItem title="Accordion Item 3">
-        <Text>This is the content of the third item.</Text>
-      </AccordionItem>
-    </View>
+    </SafeAreaView>
   );
 };
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#21212E',
+    alignItems: 'center',
+    padding: 10,
+    justifyContent: 'center',
+  },
+})
 export default Dashboard;
