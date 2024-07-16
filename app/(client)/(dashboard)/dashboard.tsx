55702a0161d0493cabdccbfa1a2e477f63cb556e
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-16 12:17:07 +0300

    endichi

--- app/(client)/(dashboard)/dashboard.tsx
+++ app/(client)/(dashboard)/dashboard.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView,} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, } from 'react-native';
 import AccordionItem from './accardion';
-
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Feather from '@expo/vector-icons/Feather';
 const Dashboard = () => {
   return (
     <SafeAreaView style={styles.container}>
-      
+      <View>
+        <Text>Главная</Text>
+        <View>
+          <FontAwesome5 name="bell" size={24} color="white" />
+          <Feather name="bookmark" size={24} color="white" />
+        </View>
+      </View>
       <AccordionItem title="Accordion Item 1">
         <Text>This is the content of the first item.</Text>
       </AccordionItem>
