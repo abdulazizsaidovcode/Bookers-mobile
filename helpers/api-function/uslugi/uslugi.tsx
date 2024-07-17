8cdf4f00b0b758a494a517d899368e773584384d
omadbekolimov <omadbekolimov96@gmail.com>
2024-07-17 17:27:51 +0500

    gooo

--- helpers/api-function/uslugi/uslugi.tsx
+++ helpers/api-function/uslugi/uslugi.tsx
@@ -4,10 +4,10 @@ import { getCategory_Client } from "@/helpers/api";
 import useGetMeeStore from '@/helpers/state_managment/getMee';
 import ClientStory from "@/helpers/state_managment/uslugi/uslugiStore";
 
-// API call to fetch categories
+// Client get all Category
 export const getAllCategory = async () => {
   try {
-    const { userLocation } = useGetMeeStore.getState(); // Corrected state access
+    const { userLocation } = useGetMeeStore.getState();
     const config = await getConfig();
     const { data } = await axios.get(`${getCategory_Client}?lat=${userLocation.coords.latitude}&lng=${userLocation.coords.longitude}`, config);
     ClientStory.getState().setAllCategory(data.body);
