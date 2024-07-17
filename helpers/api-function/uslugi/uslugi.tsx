e075e8b14939f184dd0e840075ae981b74932974
omadbekolimov <omadbekolimov96@gmail.com>
2024-07-17 12:35:47 +0500

    dkjssdjfhdsjkfdsjfhjsdfhjdsfhsdjfhdkjfhbsdjlfbhsdfjbhsdiufhgasdyufh

--- helpers/api-function/uslugi/uslugi.tsx
+++ helpers/api-function/uslugi/uslugi.tsx
@@ -5,13 +5,10 @@ import useGetMeeStore from '@/helpers/state_managment/getMee';
 
 const { userLocation, setUserLocation } = useGetMeeStore();
 
-const lat = userLocation.coords.latitude
-const long = userLocation.coords.longitude
-
 export const getAllCategory = async() =>{
     try {
         const config = await getConfig()
-        const { data } = await axios.get(`${getCategory_Client}lat=${lat}&lng=${long}`, config);
+        const { data } = await axios.get(`${getCategory_Client}`, config);
         console.log(data);
     } catch (error) {
         console.log(error)
