36841aa3018111e9b5ac091f89409e149c2a186a
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-07-17 12:17:32 +0500

    ergn

--- app/(auth)/(setPinCode)/checkInstalledPin.tsx
+++ app/(auth)/(setPinCode)/checkInstalledPin.tsx
@@ -19,7 +19,7 @@ const CheckPin: React.FC = () => {
     const { role } = registerStory()
 
     const inputs = useRef<TextInput[]>([]);
-
+    
     const { t } = useTranslation();
     const navigation = useNavigation<any>();
 
@@ -161,7 +161,8 @@ const CheckPin: React.FC = () => {
                                 key={index}
                                 style={[
                                     styles.input,
-                                    (isCorrect == null || isCorrect == false) && styles.input,
+                                    isCorrect === null  && styles.input,
+                                    isCorrect === false && styles.inputError,
                                     isCorrect === true && styles.inputSuccess,
                                 ]}
                                 value={digit}
