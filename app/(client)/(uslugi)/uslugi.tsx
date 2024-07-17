6f9918a9143a778d0244fab67ba01ca47cecc902
omadbekolimov <omadbekolimov96@gmail.com>
2024-07-17 12:32:12 +0500

    gooooo

--- app/(client)/(uslugi)/uslugi.tsx
+++ app/(client)/(uslugi)/uslugi.tsx
@@ -1,35 +1,52 @@
 import NavigationMenu from '@/components/navigation/navigation-menu';
 import { handleRefresh } from '@/constants/refresh';
 import { AntDesign } from '@expo/vector-icons';
-import { router, useRouter } from 'expo-router'; // Import the useRouter hook
-
-import React from 'react';
+import { router, useFocusEffect, useRouter } from 'expo-router'; // Import the useRouter hook
+import React, { useCallback, useEffect } from 'react';
 import { SafeAreaView, ScrollView, View, Text, TouchableOpacity, StatusBar } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Hair from './(hairHealth)/hair';
+import { getUserLocation } from '@/helpers/api-function/getMe/getMee';
+import useGetMeeStore from '@/helpers/state_managment/getMee';
+import { getAllCategory } from '@/helpers/api-function/uslugi/uslugi';
+
+
+
 
 interface Service {
-    id: any;
-    title: string;
-    distance: string;
-    icon: any;
-    onPress:any;
-  }
+  id: any;
+  title: string;
+  distance: string;
+  icon: any;
+  onPress: any;
+}
 
 
 const services = [
-  { id: 1, title: "Здоровье и красота волос", distance: "450", icon: "rightcircleo",  onPress: () => router.push("(hairHealth)/hair" ) },
-  { id: 2, title: "Ногтевой сервис", distance: "75", icon: "rightcircleo", onPress: () => router.push("./(location)/Location" ) },
-  { id: 3, title: "Ресницы и брови", distance: "322", icon: "eyeo", onPress: () => router.push("../(location)/Location" ) },
-  { id: 4, title: "Уход за телом", distance: "456", icon: "rightcircleo", onPress: () => router.push("../(location)/Location" ) },
-  { id: 5, title: "Уход за лицом", distance: "210", icon: "rightcircleo" , onPress: () => router.push("../(location)/Location" )},
-  { id: 6, title: "Наращивание волосных прядей", distance: "120", icon: "rightcircleo", onPress: () => router.push("../(location)/Location" ) },
-  { id: 7, title: "Маникюр и педикюр", distance: "180", icon: "rightcircleo", onPress: () => router.push("../(location)/Location" ) },
-  { id: 8, title: "Массаж", distance: "300", icon: "rightcircleo", onPress: () => router.push("../(location)/Location" ) }
+  { id: 1, title: "Здоровье и красота волос", distance: "450", icon: "rightcircleo", onPress: () => router.push("(hairHealth)/hair") },
+  { id: 2, title: "Ногтевой сервис", distance: "75", icon: "rightcircleo", onPress: () => router.push("./(location)/Location") },
+  { id: 3, title: "Ресницы и брови", distance: "322", icon: "eyeo", onPress: () => router.push("../(location)/Location") },
+  { id: 4, title: "Уход за телом", distance: "456", icon: "rightcircleo", onPress: () => router.push("../(location)/Location") },
+  { id: 5, title: "Уход за лицом", distance: "210", icon: "rightcircleo", onPress: () => router.push("../(location)/Location") },
+  { id: 6, title: "Наращивание волосных прядей", distance: "120", icon: "rightcircleo", onPress: () => router.push("../(location)/Location") },
+  { id: 7, title: "Маникюр и педикюр", distance: "180", icon: "rightcircleo", onPress: () => router.push("../(location)/Location") },
+  { id: 8, title: "Массаж", distance: "300", icon: "rightcircleo", onPress: () => router.push("../(location)/Location") }
 ];
 
 const ServiceCard: React.FC<Service> = ({ title, distance, icon, onPress }) => {
-  const router = useRouter(); 
+  const router = useRouter();
+
+  
+
+  useFocusEffect(
+    useCallback(() => {
+      getAllCategory()
+      return () => {}
+    },[])
+  )
+
+
+
 
   return (
     <TouchableOpacity style={tw`w-1/2 px-2 py-2`} activeOpacity={0.8} onPress={onPress}>
@@ -55,7 +72,7 @@ const Uslugi = () => {
           <Text style={[tw`font-bold text-2xl text-white mb-6`]}>Услуги</Text>
           <View style={tw`w-full flex flex-row flex-wrap mb-4`}>
             {services.map(service => (
-               <ServiceCard id={service.id} title={service.title} distance={service.distance} icon={service.icon} onPress={service.onPress}  />
+              <ServiceCard id={service.id} title={service.title} distance={service.distance} icon={service.icon} onPress={service.onPress} />
             ))}
           </View>
         </ScrollView>
