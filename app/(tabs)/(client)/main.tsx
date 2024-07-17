ef85e4d15ed0f7c20fc767900ab4d083de85b67b
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-07-17 12:53:52 +0500

    update

--- app/(tabs)/(client)/main.tsx
+++ app/(tabs)/(client)/main.tsx
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Dashboard from '@/app/(client)/(dashboard)/dashboard'
-import MasterLocations from '@/app/(client)/(map)/(master-locations)/master-locations'
 
 const HomeScreen = () => {
   return (
-	<MasterLocations/>
+	<Dashboard/>
   )
 }
 
