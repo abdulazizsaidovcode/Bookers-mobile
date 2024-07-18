50b2a920c585eabf6cbdf4d1d89d5bd167db5348
Alisher <alishersodiqov09@gmail.com>
2024-07-18 18:50:14 +0500

    edit yana

--- helpers/api-function/masters/index.ts
+++ helpers/api-function/masters/index.ts
@@ -1,21 +1,21 @@
-import { getConfig } from "@/app/(tabs)/(master)/main";
-import { base_url } from "@/helpers/api";
+import {getConfig} from "@/app/(tabs)/(master)/main";
+import {base_url} from "@/helpers/api";
 import useTopMastersStore from "@/helpers/state_managment/masters";
 import axios from "axios";
 
 export const getTopMasters = async (name?: string) => {
-  const { setTopMasters, setIsloading } = useTopMastersStore.getState();
-  setIsloading(true);
-  try {
-    const config = await getConfig();
-    const url = name
-      ? `${base_url}user/top/masters?nameOrPhone=${name}`
-      : `${base_url}user/top/masters`;
-    const { data } = await axios.get(url, config);
-    setTopMasters(data.body);
-  } catch (error) {
-    console.error("Error fetching top masters:", error);
-  } finally {
-    setIsloading(false);
-  }
+    const {setTopMasters, setIsloading} = useTopMastersStore.getState();
+    setIsloading(true);
+    try {
+        const config = await getConfig();
+        const url = name
+            ? `${base_url}user/top/masters?nameOrPhone=${name}`
+            : `${base_url}user/top/masters`;
+        const {data} = await axios.get(url, config ? config : {});
+        setTopMasters(data.body);
+    } catch (error) {
+        console.error("Error fetching top masters:", error);
+    } finally {
+        setIsloading(false);
+    }
 };
