a1be01ef6505d6dfa50de4eced28bb23591dd5ee
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-07-18 16:14:53 +0500

    34

--- app/(client)/(profile)/(profileEdit)/profileEdit.tsx
+++ app/(client)/(profile)/(profileEdit)/profileEdit.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import NavigationMenu from "@/components/navigation/navigation-menu";
@@ -16,38 +16,45 @@ import useProfileStore from "@/helpers/state_managment/client/clientEditStore";
 
 type SettingsScreenNavigationProp = NavigationProp<RootStackParamList, '(client)/(profile)/(profileEdit)/profileEdit'>;
 
+
 const ProfileEdit = () => {
   const navigation = useNavigation<SettingsScreenNavigationProp>();
   const { getMee, setGetMee } = useGetMeeStore();
-  const { birthDate, attachmentId, districtId, firstName, gender, job, lastName, nickName, phoneNumber, telegram, regionId, setAttachmentId } = useProfileStore();
-  const [isDisabled, setIsDisabled] = useState(true);
+  const {birthDate, attachmentId, districtId, firstName, gender, job, lastName, nickName, phoneNumber, telegram, regionId, setAttachmentId} = useProfileStore()
+  const [isDisabled, setIsDisabled] = useState(false)
 
   useFocusEffect(
     useCallback(() => {
-      getUser(setGetMee);
+      getUser(setGetMee)
+      return () => {};
     }, [])
   );
 
-  useEffect(() => {
-    setIsDisabled(false);
-  }, [birthDate, attachmentId, districtId, firstName, gender, job, lastName, nickName, phoneNumber, telegram, regionId]);
+  useFocusEffect(
+    useCallback(() => {
+      setIsDisabled(true)
+    }, [birthDate || attachmentId || districtId || firstName || gender || job || lastName || nickName || phoneNumber || telegram || regionId])
+  );
 
-  const putProfile = () => {
+  
+
+  const putPofile = () => {
     const data = {
-      id: getMee?.id ?? null,
-      nickName: nickName ?? getMee?.nickname ?? null,
-      firstName: firstName ?? getMee?.firstName ?? null,
-      lastName: lastName ?? getMee?.lastName ?? null,
-      phoneNumber: phoneNumber ?? getMee?.phoneNumber ?? null,
-      birthDate: birthDate ?? getMee?.birthDate ?? null,
-      gender: gender ?? getMee?.gender ?? true,
-      telegram: telegram ?? getMee?.telegram ?? null,
-      districtId: districtId ?? getMee?.districtId ?? 0,
-      attachmentId: attachmentId ?? getMee?.attachmentId ?? null,
-      job: job ?? getMee?.job ?? null
-    };
+      id: getMee && getMee.id ? getMee.id : null,
+      nickName: nickName ? nickName : getMee && getMee.nickname ? getMee.nickname : null,
+      firstName: firstName ? firstName : getMee && getMee.firstName ? getMee.firstName : null,
+      lastName: lastName ? lastName : getMee && getMee.lastName ? getMee.lastName : null,
+      phoneNumber: phoneNumber ? phoneNumber : getMee && getMee.phoneNumber ? getMee.phoneNumber : null,
+      birthDate: birthDate ? birthDate : getMee && getMee.birthDate ? getMee.birthDate : null,
+      gender: gender ? gender : true,
+      telegram: telegram ? telegram : getMee && getMee.telegram ? getMee.telegram : null,
+      districtId: districtId ? districtId : getMee && getMee.districtId ? getMee.districtId : 0,
+      attachmentId: attachmentId ? attachmentId : getMee && getMee.attachmentId ? getMee.attachmentId : null,
+      job: job ? job : getMee && getMee.job ? getMee.job : null
+    }
     console.log(data);
-  };
+    
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -57,7 +64,9 @@ const ProfileEdit = () => {
         <View style={{ marginBottom: 16 }}>
           <ProfileImgUpload
             setAttachmentId={setAttachmentId}
-            attachmentID={getMee?.attachmentId ?? null}
+            attachmentID={
+              getMee && getMee.attachmentId ? getMee.attachmentId : null
+            }
             editPin={true}
           />
           <ProfileScreen />
@@ -66,8 +75,8 @@ const ProfileEdit = () => {
         </View>
       </ScrollView>
       <View>
-        <Buttons onPress={putProfile} isDisebled={isDisabled} title="Сохранить" />
       </View>
+        <Buttons onPress={() => putPofile()}  title="Сохранить"/>
     </SafeAreaView>
   );
 };
@@ -81,9 +90,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#21212E",
   },
   genderText: {
-    color: "#fff",
+    color:"#fff",
     marginBottom: 16,
     fontSize: 20,
-    fontWeight: "500",
-  },
+    fontWeight: "500"
+  }
 });
