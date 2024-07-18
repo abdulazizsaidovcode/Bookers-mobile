06bf29916faf72be83e6bead31361585ec9629a7
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-18 15:57:10 +0300

    ishladi

--- helpers/state_managment/accardion/accardionStore.tsx
+++ helpers/state_managment/accardion/accardionStore.tsx
@@ -1,12 +1,10 @@
 import { create } from "zustand";
 interface AccardionStoreState{
-    mainExpend:boolean
     expanded:boolean,
     expanded2:boolean,
     expanded3:boolean,
     genderIndex:boolean,
     isSelected:boolean,
-    setMainExpend:(val:boolean)=>void,
     setExpanded:(val:boolean)=>void,
     setGenderIndex:(val:boolean)=>void,
     setSelection:(val:boolean)=>void,
@@ -20,8 +18,6 @@ export const  useAccardionStore=create<AccardionStoreState>((set)=>({
     isSelected:false,
     expanded2:false,
     expanded3:false,
-    mainExpend:false,
-    setMainExpend:(val:boolean)=>set({mainExpend:val}),
     setExpended2:(val:boolean)=>set({expanded2:val}),
     setExpended3:(val:boolean)=>set({expanded3:val}),
     setExpanded:(val:boolean)=>set({expanded:val}),
