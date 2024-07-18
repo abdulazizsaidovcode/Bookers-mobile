8c33ce0640c35461a76355a3365d09e5a2443c0a
Alisher <alishersodiqov09@gmail.com>
2024-07-18 19:39:33 +0500

    edit yana

--- helpers/api-function/notifications/notifications.tsx
+++ helpers/api-function/notifications/notifications.tsx
@@ -7,7 +7,7 @@ import Toast from 'react-native-simple-toast'
 export const fetchMainData = async (setMainData: (val: boolean) => void) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.get(notifications_main_data, config);
+        const { data } = await axios.get(notifications_main_data, config ? config : {});
         if (data.success) {
             setMainData(data.body);
         }
@@ -19,7 +19,7 @@ export const fetchMainData = async (setMainData: (val: boolean) => void) => {
 export const editMainDataStatus = async (isActive: boolean) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.post(`${notifications_main_data_edit}?isActive=${isActive}`, {}, config);
+        const { data } = await axios.post(`${notifications_main_data_edit}?isActive=${isActive}`, {}, config ? config : {});
         if (data.success) {
             Toast.show('Все ваши уведомления обновлены.', Toast.LONG)
         }
@@ -32,7 +32,7 @@ export const editMainDataStatus = async (isActive: boolean) => {
 export const fetchAllData = async (setOneData: (val: NotificationsAllData) => void, status: string) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.get(`${notifications_all_data}?status=${status}`, config);
+        const { data } = await axios.get(`${notifications_all_data}?status=${status}`, config ? config : {});
         if (data.success) {
             setOneData(data.body);
         }
@@ -45,7 +45,7 @@ export const fetchAllData = async (setOneData: (val: NotificationsAllData) => vo
 export const fetchAppoinmentActiveData = async (setAppoinmentActiveData: (val: boolean) => void) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.get(notifications_appointment, config);
+        const { data } = await axios.get(notifications_appointment, config ? config : {});
         if (data.success) {
             setAppoinmentActiveData(data.body)
         }
@@ -57,7 +57,7 @@ export const fetchAppoinmentActiveData = async (setAppoinmentActiveData: (val: b
 export const editMessenger = async (isMessage: boolean, goBack: void, setHasChanges: (val: boolean) => void) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.put(`${notifications_messengers_edit}?isMessage=${isMessage}`, {}, config);
+        const { data } = await axios.put(`${notifications_messengers_edit}?isMessage=${isMessage}`, {}, config ? config : {});
         if (data.success) {
             Toast.show('Ваш мессенджер успешно обновлен', Toast.LONG)
             goBack
@@ -73,7 +73,7 @@ export const editCancelOrder = async (isActive: boolean | undefined, text: strin
     const payload = { isActive, text }
     try {
         const config = await getConfig()
-        const { data } = await axios.put(notifications_cancel_edit, payload, config);
+        const { data } = await axios.put(notifications_cancel_edit, payload, config ? config : {});
         if (data.success) {
             setHasChanges(false);
             goBack
@@ -89,7 +89,7 @@ export const editChangingOrder = async (isActive: boolean | undefined, text: str
     const payload = { isActive, text }
     try {
         const config = await getConfig()
-        const { data } = await axios.put(notifications_changing_edit, payload, config);
+        const { data } = await axios.put(notifications_changing_edit, payload, config ? config : {});
         if (data.success) {
             setHasChanges(false);
             goBack
@@ -104,7 +104,7 @@ export const editChangingOrder = async (isActive: boolean | undefined, text: str
 export const editFeedbeckOrder = async (text: string | undefined, setHasChanges: (val: boolean) => void, goBack: void) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.put(notifications_feedback_edit, { text }, config);
+        const { data } = await axios.put(notifications_feedback_edit, { text }, config ? config : {});
         if (data.success) {
             setHasChanges(false);
             goBack
@@ -119,7 +119,7 @@ export const editFeedbeckOrder = async (text: string | undefined, setHasChanges:
 export const editAppoinmentOrder = async (text: string | undefined, hour: number | undefined, minute: number | undefined, isActive: boolean | undefined, goBack: void, setHasChanges: (val: boolean) => void) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.put(`${notifications_appointment_edit}?hour=${hour}&minute=${minute}&text=${text}&active=${isActive}`, {}, config);
+        const { data } = await axios.put(`${notifications_appointment_edit}?hour=${hour}&minute=${minute}&text=${text}&active=${isActive}`, {}, config ? config : {});
         if (data.success) {
             Toast.show('Ваш напоминание о встрече успешно обновлено.', Toast.LONG)
             goBack
@@ -134,7 +134,7 @@ export const editAppoinmentOrder = async (text: string | undefined, hour: number
 export const editWindowOrder = async (text: string | undefined, setHasChanges: (val: boolean) => void, goBack: void) => {
     try {
         const config = await getConfig()
-        const { data } = await axios.put(notifications_window_edit, { text }, config);
+        const { data } = await axios.put(notifications_window_edit, { text }, config ? config : {});
         if (data.success) {
             Toast.show('Ваш запрос окошка успешно обновлено.', Toast.LONG)
             goBack
