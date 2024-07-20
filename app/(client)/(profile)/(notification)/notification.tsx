855f8d351220d174f375d3800fe7cfd48b1d7a70
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-20 07:55:32 +0300

    ssss

--- app/(client)/(profile)/(notification)/notification.tsx
+++ app/(client)/(profile)/(notification)/notification.tsx
@@ -90,7 +90,6 @@ const NotificationClient = () => {
                         <Text style={{color: 'white'}}>Not found</Text>
                         <Text style={{color: 'white'}}>Not found</Text>
                         <Text style={{color: 'white'}}>Not found</Text>
-                        <Text style={{color: 'white'}}>Not found</Text>
                     </View>
                 )}
 
