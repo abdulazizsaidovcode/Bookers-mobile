4482747a62ffcc43a2829f4f79df9c87273b2462
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-20 12:15:23 +0300

    junatdim hafa bumela endi hop

--- helpers/api-function/client/clientPage.tsx
+++ helpers/api-function/client/clientPage.tsx
@@ -1,5 +1,5 @@
 import Toast from "react-native-simple-toast";
-import {getConfig} from "@/app/(tabs)/(master)/main";
+import { getConfig } from "@/app/(tabs)/(master)/main";
 import axios from "axios";
 import { client_profile_delete_url, client_profile_edit_url, getNotification_url } from "@/helpers/api";
 import { getClientNotififcations } from "@/type/client/editClient";
@@ -8,7 +8,7 @@ export const updateClientProfile = async (datas: any, navigate?: () => void, get
     try {
         if (datas) {
             const config = await getConfig()
-            const {data} = await axios.put(`${client_profile_edit_url}`, datas, config ? config : {})
+            const { data } = await axios.put(`${client_profile_edit_url}`, datas, config ? config : {})
             if (data.success) {
                 Toast.show('Successfully update', Toast.LONG)
                 navigate ? navigate() : null
@@ -21,22 +21,31 @@ export const updateClientProfile = async (datas: any, navigate?: () => void, get
     } catch (err) {
         Toast.show('An error occurred on the server', Toast.LONG)
         console.log(err);
-        
+
     }
 }
 
 
 // Profile notification function
-export const clientNotification=async (setData: (val: getClientNotififcations[]) => void) => {
+export const clientNotification = async (setData: (val: getClientNotififcations[]) => void) => {
     const config = await getConfig()
-    const ClientNotification= await axios.get(getNotification_url, config ? config : {})
-    try{
-        if(ClientNotification.data.success) setData(ClientNotification.data.body)
+    const ClientNotification = await axios.get(getNotification_url, config ? config : {})
+    try {
+        if (ClientNotification.data.success) setData(ClientNotification.data.body)
         else setData([])
     }
-    catch{
+    catch {
         Toast.show('Notification client ishlamadi', Toast.LONG)
         setData([])
+    }
+}
+export const clientNotificationDelete = async () => {
+    const config = await getConfig()
+    const clientNotificationDel = await axios.delete(getNotification_url, config ? config : {})
+    try {
+
+    }
+    catch {
     }
 }
 
