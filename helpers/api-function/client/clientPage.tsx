ae414313940d07b550744f926589b7c3b13baa44
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-07-20 09:45:58 +0500

    fv

--- helpers/api-function/client/clientPage.tsx
+++ helpers/api-function/client/clientPage.tsx
@@ -1,7 +1,7 @@
 import Toast from "react-native-simple-toast";
 import {getConfig} from "@/app/(tabs)/(master)/main";
 import axios from "axios";
-import { client_profile_edit_url, getNotification_url } from "@/helpers/api";
+import { client_profile_delete_url, client_profile_edit_url, getNotification_url } from "@/helpers/api";
 import { getClientNotififcations } from "@/type/client/editClient";
 
 export const updateClientProfile = async (datas: any, navigate?: () => void, getMe?: () => void, clearData?: () => void) => {
@@ -40,3 +40,21 @@ export const clientNotification=async (setData: (val: getClientNotififcations[])
         setData([])
     }
 }
+
+
+
+export const deleteClientProfile = async (navigate?: () => void) => {
+    try {
+        const config = await getConfig();
+        const { data } = await axios.delete(client_profile_delete_url, config ? config : {});
+        if (data.success) {
+            Toast.show('Successfully deleted', Toast.LONG);
+            navigate ? navigate() : null;
+        } else {
+            Toast.show('An error occurred on the server', Toast.LONG);
+        }
+    } catch (err) {
+        Toast.show('An error occurred on the server', Toast.LONG);
+        console.log(err);
+    }
+};
