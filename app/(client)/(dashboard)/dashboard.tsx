e119fea8b2709fa425f412364d43107e9bd0e092
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-07-24 11:23:55 +0500

    cj

--- app/(client)/(dashboard)/dashboard.tsx
+++ app/(client)/(dashboard)/dashboard.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity, ScrollView, ImageSourcePropType } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity, ScrollView, ImageSourcePropType, BackHandler } from 'react-native';
 import AccordionItem from '../../../components/accordions/accardion';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Feather from '@expo/vector-icons/Feather';
@@ -9,6 +9,9 @@ import useGetMeeStore from '@/helpers/state_managment/getMee';
 import ClientStory from '@/helpers/state_managment/uslugi/uslugiStore';
 import { getUserLocation } from '@/helpers/api-function/getMe/getMee';
 import { getAllCategory } from '@/helpers/api-function/uslugi/uslugi';
+import Toast from "react-native-simple-toast";
+
+
 // Bu bo'limga teginma
 type DashboardItemType = {
   id: number;
@@ -69,6 +72,8 @@ const Navbar: React.FC = () => (
 
 const Dashboard: React.FC = () => {
   const navigation = useNavigation<NavigationProp<any>>();
+  const [backPressCount, setBackPressCount] = useState(0);
+
   const dataDashboard: DashboardItemType[] = [
     { id: 1, image: IMAGES.health, title: 'Здоровье и красота волос', titleThen: 'Рядом с тобой 450', onPress: () => navigation.navigate('(client)/(uslugi)/(hairHealth)/hair') },
     { id: 2, image: IMAGES.nails, title: 'Ногтевой сервис', titleThen: 'Рядом с тобой 75',onPress: () => navigation.navigate('(client)/(uslugi)/(hairHealth)/hair') },
@@ -76,6 +81,39 @@ const Dashboard: React.FC = () => {
     { id: 4, image: IMAGES.body, title: 'Уход за телом', titleThen: 'Рядом с тобой 456',onPress: () => navigation.navigate('(client)/(uslugi)/(hairHealth)/hair') },
     { id: 5, image: IMAGES.face, title: 'Уход за лицом', titleThen: 'Рядом с тобой 210',onPress: () => navigation.navigate('(client)/(uslugi)/(hairHealth)/hair') },
   ];
+
+  // navigatsiyani login registratsiyadan o'tganda bloklash
+useEffect(() => {
+  const unsubscribe = navigation.addListener('beforeRemove', (e: any) => {
+    e.preventDefault();
+  });
+
+  return unsubscribe;
+}, [navigation]);
+
+// 2 marta orqaga qaytishni bosganda ilovadan chiqaradi
+useFocusEffect(
+  useCallback(() => {
+    const onBackPress = () => {
+      if (backPressCount === 0) {
+        setBackPressCount(backPressCount + 1);
+        Toast.show('Orqaga qaytish uchun yana bir marta bosing', Toast.SHORT);
+        setTimeout(() => {
+          setBackPressCount(0);
+        }, 2000); // 2 soniya ichida ikkinchi marta bosilmasa, holatni qayta boshlaydi
+        return true; // Orqaga qaytishni bloklaydi
+      } else {
+        BackHandler.exitApp(); // Ilovadan chiqish
+        return false;
+      }
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+    return () => BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+  }, [backPressCount])
+);
+
 
   return (
     <SafeAreaView style={styles.container}>
