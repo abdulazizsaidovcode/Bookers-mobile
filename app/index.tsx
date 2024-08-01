69418bec67435dfc65c9dcafce6a3dae3ab5075b
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-08-01 12:25:07 +0500

    43

--- app/index.tsx
+++ app/index.tsx
@@ -6,12 +6,19 @@ import CheckPinOnCome from "./(auth)/(checkPinCode)/checkPinCode";
 import InstallPin from "./(auth)/(setPinCode)/installPin";
 import { useNavigation } from "expo-router";
 import { Loading } from "@/components/loading/loading";
+import * as SplashScreen from 'expo-splash-screen';
 
 const Index: React.FC = () => {
   const [isFirstLaunch, setIsFirstLaunch] = useState<null | boolean>(null);
   const navigation = useNavigation<any>();
 
   useEffect(() => {
+    async function prepare() {
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      await SplashScreen.hideAsync();
+    }
+
+    prepare();
     const checkFirstLaunch = async () => {
       try {
         const number = await SecureStore.getItemAsync("number");
