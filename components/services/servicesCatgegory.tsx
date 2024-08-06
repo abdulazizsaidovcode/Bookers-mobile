4980bc9954a5b4aed898222f34cb0adcba8712af
omadbekolimov <omadbekolimov96@gmail.com>
2024-08-06 12:49:21 +0500

    gooooooooo

--- components/services/servicesCatgegory.tsx
+++ components/services/servicesCatgegory.tsx
@@ -32,7 +32,7 @@ const ServicesCategory: React.FC<ServicesProps> = ({
 
     return (
         <TouchableOpacity
-            activeOpacity={0.7}
+            activeOpacity={1}
             onPress={() => {
                 handlePress();   
             }}
@@ -40,31 +40,38 @@ const ServicesCategory: React.FC<ServicesProps> = ({
             <View style={[tw`rounded-2xl`, { backgroundColor: "#21212E" }]}>
                 <View
                     style={[
-                        tw`flex p-3 mb-3 flex-row rounded-2xl items-center`,
-                        { backgroundColor: "#B9B9C9" },
+                        tw`flex p-3 mb-3 flex-row  items-center`,
+                        {backgroundColor:"#B9B9C9",borderRadius:18}
+                        
                     ]}
                 >
                     <View
                         style={[
                             tw`rounded-full`,
                             {
-                                backgroundColor: checked ? "red" : "#B9B9C9",
+                                backgroundColor:"#B9B9C9"
+                                // backgroundColor: checked ? "red" : "#B9B9C9",
                             },
                         ]}
                     >
                         {isRadioButton ? (
+                            <View style={styles.checkboxContainer}>
                             <RadioButton
                                 value="radio"
                                 status={checked ? "checked" : "unchecked"}
                                 onPress={handlePress}
-                                color={checked ? "white" : "black"} // RadioButton color change
+                                color={checked ? "#9C0A35" : "black"} // RadioButton color change
                             />
+                            </View>
                         ) : (
-                            <Checkbox
+                            <View style={styles.checkboxContainer}>
+                                <Checkbox
                                 status={checked ? "checked" : "unchecked"}
                                 onPress={handlePress}
-                                color={checked ? "white" : "black"} // Checkbox color change
+                                color={checked ? "#9C0A35" : "black"} // Checkbox color change
                             />
+                            </View>
+                            
                         )}
                     </View>
                     <Text style={tw`text-black text-lg font-bold ml-2`}>{title}</Text>
@@ -77,6 +84,10 @@ const ServicesCategory: React.FC<ServicesProps> = ({
 const styles = StyleSheet.create({
     checkbox: {
         alignSelf: "center",
+    },
+    checkboxContainer: {
+        transform: [{ scale: 1.1 }],
+        backgroundColor:'#B9B9C9'
     },
 });
 
